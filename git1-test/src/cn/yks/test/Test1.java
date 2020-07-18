ac30463f8439b9901b7afdebc0f03309bfc13535
admin <admin@qq.com>
2020-07-18 23:29:40 +0800

    这是一个克隆提交的测试

--- git1-test/src/cn/yks/test/Test1.java
+++ git1-test/src/cn/yks/test/Test1.java
@@ -9,11 +9,13 @@ public class Test1 {
         System.out.println("这是一个测试类");
         System.out.println("这是一个测试类");
         System.out.println("这是我的另一个测试语句");
+        //hello test
     }
 
     public void show(){
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
+            System.out.println("这是克隆的测试语句");
         }
     }
 }
