6bbb237cf71c4680d762b820e72f2e0ac0666c40
admin <admin@qq.com>
2020-07-18 23:23:20 +0800

    这是一个测试类项目

---
+++ git1-test/src/cn/yks/test/Test1.java
@@ -0,0 +1,19 @@
+package cn.yks.test;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("这是一个测试类");
+        System.out.println("这是一个测试类");
+        System.out.println("这是一个测试类");
+        System.out.println("这是一个测试类");
+        System.out.println("这是一个测试类");
+        System.out.println("这是一个测试类");
+
+    }
+
+    public void show(){
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
